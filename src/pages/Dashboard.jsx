4bcc5337e43dd9c8ac3b4b867545bda1ba34edba
voanhkiet <voanhkiet261192@gmail.com>
2026-04-05 14:21:49 +0700

    fix upload button

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -35,10 +35,23 @@ export default function Dashboard() {
     <h2 className="text-3xl font-bold mb-6">Dashboard</h2>
 
     <div className="bg-white p-6 rounded-2xl shadow mb-8 flex gap-4 items-center">
-      <input className="border p-2 rounded-lg" type="file" />
-<input className="border p-2 rounded-lg" placeholder="Title" />
+      <input
+  type="file"
+  className="border p-2 rounded-lg"
+  onChange={(e) => setFile(e.target.files[0])}
+/>
 
-<button className="bg-black text-white px-6 py-2 rounded-lg hover:bg-gray-800 transition">
+<input
+  className="border p-2 rounded-lg"
+  placeholder="Title"
+  value={title}
+  onChange={(e) => setTitle(e.target.value)}
+/>
+
+<button
+  onClick={handleUpload}
+  className="bg-black text-white px-6 py-2 rounded-lg hover:bg-gray-800 transition"
+>
   Upload
 </button>
     </div>
@@ -50,7 +63,10 @@ export default function Dashboard() {
             src={img.image_url}
             className="w-full h-40 object-cover rounded"
           />
-          <button className="mt-2 w-full bg-red-500 text-white py-1 rounded">
+          <button
+            onClick={() => handleDelete(img.id)}
+            className="mt-2 w-full bg-red-500 text-white py-1 rounded"
+          >
             Delete
           </button>
         </div>
