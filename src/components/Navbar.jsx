34891a082a814864b3d909a64f96cffad1b6ddfd
voanhkiet <voanhkiet261192@gmail.com>
2026-04-06 15:07:51 +0700

    update UI for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,7 +66,7 @@ export default function Navbar({ token, setToken }) {
     
  {open && (
   <div
-    className="fixed inset-0 bg-black/40 z-40"
+    className="fixed inset-0 bg-black/60 z-40"
     onClick={() => setOpen(false)}
   />
 )}   
@@ -74,7 +74,7 @@ export default function Navbar({ token, setToken }) {
       {/* 📱 MOBILE MENU */}
 {/* 📱 MOBILE MENU */}
 <div
-  className={`fixed inset-0 flex justify-center items-start pt-16 z-50 transition-all duration-300 ${
+  className={`fixed inset-0 flex justify-center items-start pt-14 z-50 transition-all duration-300 ${
     open ? "opacity-100" : "opacity-0 pointer-events-none"
   }`}
 >
@@ -85,10 +85,10 @@ export default function Navbar({ token, setToken }) {
   />
 
   {/* MENU PANEL */}
-  <div className="relative w-[90%] md:w-[600px] max-w-md bg-black text-white rounded-2xl shadow-[0_20px_60px_rgba(0,0,0,0.6)] overflow-hidden animate-slideDown">
+  <div className="relative w-[90%] md:w-[600px] max-w-md bg-black text-white rounded-2xl shadow-[0_10px_40px_rgba(0,0,0,0.8)] overflow-hidden animate-slideDown">
 
     {/* HEADER */}
-    <div className="flex justify-between items-center px-6 py-5 border-b border-gray-800">
+    <div className="flex justify-between items-center px-6 py-4 border-b border-gray-800">
       <h1 className="text-xl font-bold">🎨 Art Gallery</h1>
       <button onClick={() => setOpen(false)} className="text-2xl">
         ✖
@@ -100,8 +100,7 @@ export default function Navbar({ token, setToken }) {
 
       <Link
         to="/"
-        className="py-4 border-b border-gray-800 hover:text-gray-400"
-        onClick={() => setOpen(false)}
+        className="py-4 border-b border-gray-800 hover:text-gray-400 hover:translate-x-1 transition"        onClick={() => setOpen(false)}
       >
         Gallery
       </Link>
