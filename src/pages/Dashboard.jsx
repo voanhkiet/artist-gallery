d3d6effcce138cf92a46289d4c3bbb91261be57a
voanhkiet <voanhkiet261192@gmail.com>
2026-04-09 16:00:01 +0700

    import navigate

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { uploadImage, getImages, deleteImage } from "../services/api";
-
+import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
   const navigate = useNavigate();
